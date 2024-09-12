b3e383de85d8f126c41e3db662bed07247703331
Ross Parker <>
2024-09-12 13:25:52 +0800

    v1.3.00

--- Class Enrolment/CHANGEDB.php
+++ Class Enrolment/CHANGEDB.php
@@ -5,33 +5,41 @@ $sql = array();
 $count = 0;
 
 //v1.0.00
+$count++;
 $sql[$count][0] = "1.0.00";
 $sql[$count][1] = "-- First version, nothing to update";
-$count++;
+
 
 //v1.0.01
+$count++;
 $sql[$count][0] = "1.0.01";
 $sql[$count][1] = "";
-$count++;
 
 //v1.0.02
+$count++;
 $sql[$count][0] = "1.0.02";
 $sql[$count][1] = "";
-$count++;
 
 //v1.1.00
+$count++;
 $sql[$count][0] = "1.1.00";
 $sql[$count][1] = "";
-$count++;
 
 //v1.1.01
+$count++;
 $sql[$count][0] = "1.1.01";
 $sql[$count][1] = "";
-$count++;
 
 //v1.2.00
+$count++;
 $sql[$count][0] = "1.2.00";
 $sql[$count][1] = "
 INSERT INTO `gibbonSetting` (`gibbonSettingID` ,`scope` ,`name` ,`nameDisplay` ,`description` ,`value`) VALUES (NULL , 'Class Enrolment', 'useDatabaseLocking', 'Use Database Locking', 'Ensures fidelity of minimum and maximum enrolment, but comes with a performance cost.', 'Y');end
 ";
-$count++;
+
+//v1.3.00
+++$count;
+$sql[$count][0] = '1.3.00';
+$sql[$count][1] = "
+UPDATE gibbonModule SET author='Gibbon Foundation', url='https://gibbonedu.org' WHERE name='Class Enrolment';end
+";
--- Class Enrolment/manifest.php
+++ Class Enrolment/manifest.php
@@ -1,7 +1,9 @@
 <?php
 /*
-Gibbon, Flexible & Open School System
-Copyright (C) 2010, Ross Parker
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,9 +25,9 @@ $description = "A simple module for allowing parents to enrol their children in 
 $entryURL = "enrolment.php";
 $type = "Additional";
 $category = "Learn";
-$version = "1.2.00";
-$author = "Ross Parker";
-$url = "http://rossparker.org";
+$version = "1.3.00";
+$author = "Gibbon Foundation";
+$url = "https://gibbonedu.org";
 
 //Settings
 $gibbonSetting[] = "INSERT INTO `gibbonSetting` (`gibbonSettingID` ,`scope` ,`name` ,`nameDisplay` ,`description` ,`value`) VALUES (NULL , 'Class Enrolment', 'openParentEnrolment', 'Open Parent Enrolment', 'When should parents sign ups open?', '');";
